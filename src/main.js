c361af41f2c89cec030222783b3f4528a1394de6
karl-denby <karl.denby@gmail.com>
2019-04-23 23:34:33 +0100

    video 83 of 177

--- src/main.js
+++ src/main.js
@@ -1,53 +1,28 @@
 import Vue from 'vue';
 import './style.scss';
-import genres from './util/genres'
+import MovieList from './components/MovieList.vue'
+import MovieFilter from './components/MovieFilter.vue'
 
 new Vue({
     el: '#app',
-    components: {
-        'movie-list': {
-            template: 
-                `<div id="movie-list"> 
-                    <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
-                </div>`,
-            data: function () {
-                return {
-                    movies: [
-                        { title: 'Pulp Fiction'},
-                        { title: 'Austin Powers'},
-                        { title: 'Requiem for a dream'}
-                    ]
+    data: {
+        genre: [],
+        time: []
+    },
+    methods: {
+        checkFilter (category, title, checked) {
+            if (checked) {
+                this[category].push(title)
+            } else {
+                let index = this[category].indexOf(title)
+                if (index > -1) {
+                    this[category].splice(index, 1)
                 }
             }
-        },
-        'movie-filter': {
-            data() {
-                return {
-                    genres
-                }
-            },
-            template: 
-                `<div id="movie-filter"> 
-                    <h2>Filter Results</h2>
-                    <div>
-                        <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
-                    </div>
-                </div>`,
-            components: {
-                'check-filter': {
-                    data: function () {
-                        return {
-                            checked: false
-                        }
-                    },
-                    props: [ 'title' ],
-                    template: 
-                    `<div class="check-filter" v-on:click="checked = !checked">
-                        <span class="checkbox"></span>
-                        <span class="check-filter-title">{{ title }}</span>
-                    </div>`
-                }
-            }    
         }
     },
+    components: {
+        MovieList,
+        MovieFilter            
+    }
 })
